cf70a0e3b0c1b162cd5192da37aea974d4f1e081
esveraldo <esveraldo@hotmail.com>
2022-05-07 19:14:29 -0300

    Implantando front end para testar Api

---
+++ BACKEND/src/CommerceTest.Application/Dtos/UserDto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CommerceTest.Application.Dtos
+{
+    public class UserDto
+    {  
+        [Required(ErrorMessage ="O campo {0} é obrigatório.")]
+        public string? UserName { get; set; }
+        [Required(ErrorMessage = "O campo {0} é obrigatório.")]
+        public string? Password { get; set; }
+        public string? ConfirmPassword { get; set; }
+    }
+}
---
+++ BACKEND/src/CommerceTest.Infra/CommerceTeste.Infra/Services/Contracts/IUserService.cs
@@ -0,0 +1,14 @@
+﻿using CommerceTest.Application.Dtos;
+
+namespace CommerceTeste.Infra.Services.Contracts
+{
+    public interface IUserService
+    {
+        Task<IEnumerable<UserDto>> ObterTodosOsUsuarios();
+        Task<UserDto> ObterUsuarioPorId(Guid id);
+        Task<UserDto> SalvarRegistroDoUsuario(UserDto userDto);
+        Task<UserDto> AlterarRegistroDoUsuario(UserDto userDto);
+        Task<UserDto> DeletarRegistroDoUsuario(Guid id);
+        bool ValidaSenha(string user, string pass);
+    }
+}
